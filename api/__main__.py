92beaf0111378f6b8a664adb587b92b258fcb3a1
Nishant Sapkota <snishant306@gmail.com>
2022-04-21 22:28:17 +0545

    refactor: include auth router into the api

--- api/__main__.py
+++ api/__main__.py
@@ -4,10 +4,20 @@ import uvicorn
 from fastapi import FastAPI
 from tortoise import Tortoise
 
+from api.core.auth.views import router as auth
 from api.core.books.views import router as books
 from api.core.tortoise_config import tortoise_config
 
 logger = logging.getLogger("main")
+
+tags = [
+    {"name": "Books", "description": "All the operations related to books"},
+    {
+        "name": "Authentication",
+        "description": "All the operations related to user authentication",
+    },
+]
+
 app = FastAPI(
     title="Library Management System - Backend",
     docs_url="/",
@@ -18,9 +28,11 @@ app = FastAPI(
         "url": "https://thenishantsapkota.github.io",
         "email": "snishant306@gmail.com",
     },
+    openapi_tags=tags,
 )
 
 app.include_router(books, prefix="/api/v1/books")
+app.include_router(auth, prefix="/api/v1/auth")
 
 
 @app.on_event("startup")
