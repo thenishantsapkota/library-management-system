d70b3d779d7da2b139cb890520459828014958a1
Nishant Sapkota <snishant306@gmail.com>
2022-04-25 18:58:13 +0545

    refactor: add error handling for the api

--- api/__main__.py
+++ api/__main__.py
@@ -1,7 +1,10 @@
 import logging
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from sqlalchemy import JSON
+from starlette.requests import Request
 from tortoise import Tortoise
 
 from api.core.auth.views import router as auth
@@ -11,11 +14,11 @@ from api.core.tortoise_config import tortoise_config
 logger = logging.getLogger("main")
 
 tags = [
-    {"name": "Books", "description": "All the operations related to books"},
     {
         "name": "Authentication",
         "description": "All the operations related to user authentication",
     },
+    {"name": "Books", "description": "All the operations related to books"},
 ]
 
 app = FastAPI(
@@ -31,14 +34,28 @@ app = FastAPI(
     openapi_tags=tags,
 )
 
-app.include_router(books, prefix="/api/v1/books")
-app.include_router(auth, prefix="/api/v1/auth")
+app.include_router(auth, prefix="/auth")
+app.include_router(books, prefix="/books")
 
 
 @app.on_event("startup")
 async def on_startup():
     await Tortoise.init(tortoise_config)
     logger.debug("Connected to Database!")
+
+
+@app.exception_handler(Exception)
+async def handle_exceptions(_, err: Exception):
+    return JSONResponse(
+        content={"success": False, "message": str(err)}, status_code=400
+    )
+
+
+@app.exception_handler(HTTPException)
+async def handle_http_exception(_, err: HTTPException):
+    return JSONResponse(
+        content={"success": False, "message": err.detail}, status_code=err.status_code
+    )
 
 
 if __name__ == "__main__":
