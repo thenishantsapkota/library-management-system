b2f829be41ada87787d7fc1b5f6cd74ecc34ea9b
Nishant Sapkota <snishant306@gmail.com>
2022-04-29 22:13:28 +0545

    refactor: made some changes to the User model

--- api/core/auth/models.py
+++ api/core/auth/models.py
@@ -2,14 +2,19 @@ from tortoise import Model, fields
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 
-class User(Model):
+class UserModel(Model):
     id = fields.IntField(pk=True, index=True)
     username = fields.CharField(unique=True, max_length=200)
     email = fields.CharField(unique=True, max_length=200)
     full_name = fields.TextField(null=True)
     password = fields.TextField()
+    is_superuser = fields.BooleanField(null=True, default=False)
 
 
-User_Pydantic = pydantic_model_creator(User, name="User")
-UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)
-UserOut_Pydantic = pydantic_model_creator(User, name="UserOut", exclude=("password",))
+User_Pydantic = pydantic_model_creator(UserModel, name="User")
+UserIn_Pydantic = pydantic_model_creator(
+    UserModel, name="UserIn", exclude_readonly=True
+)
+UserOut_Pydantic = pydantic_model_creator(
+    UserModel, name="UserOut", exclude=("password", "role")
+)
