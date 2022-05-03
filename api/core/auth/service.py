6afffb60796dce91817932e4b9432caa19b7b9ba
Nishant Sapkota <snishant306@gmail.com>
2022-05-03 23:00:24 +0545

    chore: black formatting

--- api/core/auth/service.py
+++ api/core/auth/service.py
@@ -7,10 +7,11 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
 
 class AuthService:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
     @classmethod
     def verify_password(cls, password: str, hash: str):
